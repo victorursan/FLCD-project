cabdce838b11045f03811325fbc30faef66bf8e1
victorursan <victor.ursan@gmail.com>
2017-01-16 14:37:17 +0200

    added first and follow

--- src/com/FLCD/controller/controller.py
+++ src/com/FLCD/controller/controller.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 from src.com.FLCD.model.file_repo import FileRepo
 
@@ -9,17 +10,27 @@ class Controller(object):
         self.__state_closure = []
         self.__computed_closure = []
         self.__goto_state = {}
+        self.__the_table = {}
+        self.__follow = defaultdict(list)
 
     def run(self):
         self.__productions = self.find_productions()
-
+        print(self.join_productions(self.__productions))
         start_point = [(self.__start, self.add_dot(self.__productions[self.__start][0]))]
         self.__state_closure.append(start_point)
         self.__computed_closure.append(self.compute_closure(start_point))
 
         self.canonical_collection_of_states()
 
-        return self.__state_closure, self.__computed_closure, self.__goto_state
+        print(self.follow())
+
+        for i in range(max(self.__goto_state.values()) + 1):
+            for j in ["$"] + self.__non_terminals + self.__terminals:
+                self.__the_table[(i, j)] = self.get_shift(i, j) if not self.has_final(i) else -2  # todo
+        return self.__state_closure, self.__computed_closure, self.__goto_state, self.__the_table
+
+    def get_shift(self, i, j):
+        return self.__goto_state[(i, j)] if (i, j) in self.__goto_state.keys() else -1
 
     def canonical_collection_of_states(self, poz=0):
         to_do = []
@@ -48,6 +59,9 @@ class Controller(object):
                 new_val.append(n_v)
             new_productions[k] = new_val
         return new_productions
+
+    def has_final(self, prod):
+        return any([self.is_finished(x[1]) for x in self.__computed_closure[prod]])
 
     @staticmethod
     def add_dot(lst):
@@ -89,4 +103,72 @@ class Controller(object):
 
     @staticmethod
     def join_productions(productions):
-        return [k + " -> " + " | ".join([",".join(e) for e in v]) for k, v in productions.items()]
+        return [k + " -> " + " | ".join(["".join(e) for e in v]) for k, v in productions.items()]
+
+    def first(self):
+        first_map = {"#": set("#")}
+
+        for terminal in self.__terminals:
+            first_map[terminal] = {terminal}
+
+        for non_terminal in self.__non_terminals:
+            values = set()
+            for prod in self.__productions[non_terminal]:
+                first = prod[0]
+                if first in self.__terminals or first == "#":
+                    values.add(first)
+            first_map[non_terminal] = values
+
+        changed = True
+        while changed:
+            changed = False
+            for non_terminal in self.__non_terminals:
+                for rhs in self.__productions[non_terminal]:
+                    if all([len(first_map[e]) > 0 for e in rhs]):
+                        vals = set()
+                        for token in rhs:
+                            vals |= first_map[token]
+                            if "#" not in first_map[token]:
+                                if "#" in vals:
+                                    vals.remove("#")
+                                break
+                        if not vals.issubset(first_map[non_terminal]):
+                            first_map[non_terminal] |= vals
+                            changed = True
+
+        return first_map
+
+    @staticmethod
+    def first_sq(firsts, sq):
+        result = {"#"}
+        for token in sq:
+            result |= firsts[token]
+            if "#" not in firsts[token]:
+                if "#" in result:
+                    result.remove("#")
+                break
+        return result
+
+    def follow(self):
+        first_map = self.first()
+        follow_map = defaultdict(set)
+        follow_map[self.__start] = {"#"}
+        changed = True
+        while changed:
+            changed = False
+            for non_terminal in self.__non_terminals:
+                for k, v in self.__productions.items():
+                    for rhs in v:
+                        if non_terminal in rhs:
+                            sub_sq = rhs[rhs.index(non_terminal) + 1:]
+                            first_sq = self.first_sq(first_map, sub_sq)
+                            first_sq_noeps = filter(lambda x: x != "#", first_sq)
+
+                            if not set(first_sq_noeps).issubset(follow_map[non_terminal]):
+                                follow_map[non_terminal] |= set(first_sq_noeps)
+                                changed = True
+                            if "#" in first_sq:
+                                if not set(follow_map[k]).issubset(follow_map[non_terminal]):
+                                    follow_map[non_terminal] |= follow_map[k]
+                                    changed = True
+        return follow_map
--- src/com/FLCD/main.py
+++ src/com/FLCD/main.py
@@ -2,7 +2,9 @@ from src.com.FLCD.controller.controller import Controller
 
 if __name__ == '__main__':
     ctrl = Controller()
-    state_closure, computed_closure, goto_state = ctrl.run()
+    state_closure, computed_closure, goto_state, the_table = ctrl.run()
     print(state_closure)
     print(computed_closure)
     print(goto_state)
+    print(the_table)
+
